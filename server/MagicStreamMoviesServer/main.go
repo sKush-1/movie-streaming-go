5f715c01f3740a9ec70b85e6641b23ae525d1ad3
sKush-1 <kushsumit27@gmail.com>
2025-10-05 14:17:54 +0000

    create movies model

---
+++ server/MagicStreamMoviesServer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello World")
+	})
+
+	if err := router.Run(":8080"); err!=nil {
+		fmt.Println("Failed to start server",err)
+	}
+
+}

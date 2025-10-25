77b9e2ec8b43072d3b6561afeda44194e3c5a561
sKush-1 <kushsumit27@gmail.com>
2025-10-25 13:39:38 +0530

    add open ai key

--- server/MagicStreamMoviesServer/controllers/movie_controller.go
+++ server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -2,11 +2,16 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
 	"github.com/sKush-1/magic_stream_movies_server/database"
 	"github.com/sKush-1/magic_stream_movies_server/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -95,4 +100,101 @@ func AddMovie() gin.HandlerFunc {
 		return
 
 	}
+}
+
+func GetReviewRankings(admin_review string) (models.Ranking, error) {
+	rankings, err := GetRankings()
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	sentimentDelimited := ""
+
+	for _, ranking := range rankings {
+		if ranking.RankingName != "999" {
+			sentimentDelimited += ranking.RankingName + ","
+		}
+	}
+
+	sentimentDelimited = strings.Trim(sentimentDelimited, ",")
+
+	err = godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	OpenAIAPIKey := os.Getenv("OPENAI_API_KEY")
+
+	if OpenAIAPIKey == "" {
+		return "", 0, errors.New("OPENAI_API_KEY is not set in environment variables")
+	}
+
+	llm, err := openai.New(openai.WithToken(OpenAIAPIKey))
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	base_prompt_template := os.Getenv("BASE_PROMPT_TEMPLATE")
+
+
+	return sentimentDelimited, 1, nil
+}
+
+
+
+
+
+}
+
+func AdminReviewUpdate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		movieId := c.Param("imdb_id")
+
+		if movieId == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var req struct {
+			AdminReview string `json:"admin_review"`
+		}
+
+		var resp struct {
+			RankingName string `json:"ranking_name"`
+			AdminReview string `json:"admin_review"`
+		}
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+	}
+}
+
+func GetRankings() ([]models.Ranking, error) {
+	var rankings []models.Ranking
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var rankingCollection *mongo.Collection = database.OpenCollection("rankings")
+
+	cursor, err := rankingCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+
+	if err := cursor.All(ctx, &rankings); err != nil {
+		return nil, err
+	}
+
+	return rankings, nil
+
 }
